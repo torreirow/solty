ebd33dc9a5f2e887debdc9db3b16e4731f2d1119
TorreiroW <torreirow@dutchyland.net>
2026-04-01 20:02:54 +0200

    release: bump version to 0.3.0
    Update VERSION and CHANGELOG.md for minor release.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,7 +5,7 @@ All notable changes to Soltty will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
-## NEXT VERSION
+## 0.3.0 - 01 Apr 2026
 
 ### Added
 - **List clients subcommand**: New `soltty list clients` shows all clients alphabetically with project counts
--- flake.nix
+++ flake.nix
@@ -23,7 +23,7 @@
 
             # vendorHash is automatically updated by release.sh when Go dependencies change.
             # To manually update: Run `nix build .#soltty 2>&1 | grep "got:"` and use the hash shown.
-            vendorHash = "sha256-eKeUhS2puz6ALb+cQKl7+DGvm9Cl+miZAHX0imf9wdg=";
+            vendorHash = "sha256-Wr+GuQ7AbD0RmHppiLU+6GxRl8FRqp7yht8mnZak/2A=";
 
             ldflags = [
               "-s"
